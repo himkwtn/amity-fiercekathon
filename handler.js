a800abcda3c885dcb9faf762516aac8025081c2b
himkwtn <him10424@gmail.com>
2022-05-17 10:19:15 +0700

    clean up

--- handler.js
+++ handler.js
@@ -70,20 +70,6 @@ module.exports.migrateWorker = async (event) => {
   }
 };
 
-module.exports.createUser = async (event) => {
-  try {
-    const { apiKey, userId, displayName } = JSON.parse(event.body);
-    const userService = new UserService(apiKey);
-    const result = await userService.createUser(userId, displayName);
-    return {
-      statusCode: 200,
-      body: JSON.stringify(result),
-    };
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
-};
 module.exports.createUserWorker = async (event) => {
   try {
     for (const message of event.Records) {
@@ -97,40 +83,23 @@ module.exports.createUserWorker = async (event) => {
     throw e;
   }
 };
-module.exports.createUserQueue = async (event) => {
-  try {
-    const { apiKey, userId, displayName } = JSON.parse(event.body);
-    const result = await sendMessage({ apiKey, userId, displayName });
+
+module.exports.createPostWorker = async (event) => {
+  for (const message of event.Records) {
+    const { apiKey, userId, postId, text, attachments } = JSON.parse(
+      message.body
+    );
+    const userService = new UserService(apiKey);
+    const { accessToken } = await userService.createUser(userId);
+    const postService = new PostService(apiKey, accessToken);
+    const result = await postService.createPost(
+      text,
+      postId,
+      attachments?.images
+    );
     return {
       statusCode: 200,
       body: JSON.stringify(result),
     };
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
-};
-
-const createPost = async (event) => {
-  const { apiKey, userId, postId, text, attachments } = JSON.parse(event.body);
-  const userService = new UserService(apiKey);
-  const { accessToken } = await userService.createUser(userId);
-  const postService = new PostService(apiKey, accessToken);
-  const result = await postService.createPost(
-    text,
-    postId,
-    attachments?.images
-  );
-  return {
-    statusCode: 200,
-    body: JSON.stringify(result),
-  };
-};
-
-module.exports.createPost = createPost;
-
-module.exports.createPostWorker = async (event) => {
-  for (const message of event.Records) {
-    await createPost(message);
   }
 };
